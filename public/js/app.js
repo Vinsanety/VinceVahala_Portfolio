c8210e21709e765c54e136abe3136f6ed47d6d01
vince vahala <vincevahala@gmail.com>
2016-08-31 21:41:27 -0600

    all angular routes set up. Creating necessary views pages. App and controller js are set up. Minor improvements. Materialize files included locally to have navbar set up.

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,28 @@ var app = angular.module('Vinsanety', ['ui.router'])
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/')
   $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/views/home.html',
-      controller: 'homeController',
-      controllerAs: 'home'
-    })
-    // .state('xyz', {
-    //   url: '/xyz',
-    //   templateUrl: '/xyz/cart.html',
-    //   controller: 'xyzController',
-    //   controllerAs: 'bio'
-    // })
+  .state('home', {
+    url: '/home',
+    templateUrl: '/views/home.html',
+    controller: 'homeController',
+    controllerAs: 'home'
+  })
+  .state('resume', {
+    url: '/resume',
+    templateUrl: '/views/resume.html',
+    controller: 'resumeController',
+    controllerAs: 'resume'
+  })
+  .state('projects', {
+    url: '/projects',
+    templateUrl: '/views/projects.html',
+    controller: 'projectsController',
+    controllerAs: 'projects'
+  })
+  .state('mountaineering', {
+    url: '/mountaineering',
+    templateUrl: '/views/mountaineering.html',
+    controller: 'mountaineeringController',
+    controllerAs: 'mountaineering'
+  })
 }])
---
+++ public/js/controllers.js
@@ -0,0 +1,18 @@
+app.controller('homeController', function($scope) {
+    console.log('home route hit');
+
+})
+
+app.controller('resumeController', function($scope) {
+    console.log('resume route hit');
+
+})
+
+app.controller('projectsController', function($scope) {
+    console.log('projects route hit');
+
+})
+
+app.controller('mountaineeringController', function($scope) {
+    console.log('mountaineering route hit');
+})

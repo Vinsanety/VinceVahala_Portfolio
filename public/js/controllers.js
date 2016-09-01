e7f864282df6bba9eb68b66b09d445911e365109
vince vahala <vincevahala@gmail.com>
2016-08-31 22:13:37 -0600

    cleaned up the routing pages. added the logic to the controllers page for the mobile menu.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,18 +1,22 @@
 app.controller('homeController', function($scope) {
-    console.log('home route hit');
+  $(".button-collapse").sideNav();
+  console.log('home route hit');
 
 })
 
 app.controller('resumeController', function($scope) {
-    console.log('resume route hit');
+  $(".button-collapse").sideNav();
+  console.log('resume route hit');
 
 })
 
 app.controller('projectsController', function($scope) {
-    console.log('projects route hit');
+  $(".button-collapse").sideNav();
+  console.log('projects route hit');
 
 })
 
 app.controller('mountaineeringController', function($scope) {
-    console.log('mountaineering route hit');
+  $(".button-collapse").sideNav();
+  console.log('mountaineering route hit');
 })

339f70a462cb4420096935d4999e33d5bd630923
vince vahala <vincevahala@gmail.com>
2019-07-18 19:26:01 -0600

    Updated color palette and updated colors throughout site. Added a new hover state for the nav, js for active state. Updated bio and various other pages cleanup. Cleanup

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,8 +1,21 @@
 $( document ).ready(function(){
-  $('.sidenav')
+  // Instantiate Sidenav
+  $(".sidenav")
+  // Close Sidenav
   .sidenav()
-  .on('click tap', 'li a', () => {
-      $('.sidenav').sidenav('close');
+  .on("click tap", "li a", () => {
+      $(".sidenav").sidenav("close");
   });
-  $('.tabindex-hidden-link').attr('tabindex', '-1');
+  // Removes Tab Index for Accessibility
+  $(".tabindex-hidden-link").attr("tabindex", "-1");
+  // Adds Active Class to Nav Items
+  $(".main-nav-link").on("click", function(){
+    var clicked = $(this);
+    $(".main-nav-link").each(function(){
+      if($(this).hasClass("active-link")){
+        $(this).removeClass("active-link");
+      }
+    });
+    $(this).addClass("active-link");
+  });
 });
